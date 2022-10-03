a6f0e62b8c4c4c5b598e455c93b6272a212a5b98
Georgios Makridis <gmakridis@unipi.gr>
2022-10-03 18:41:48 +0300

    adding Saliency Map explainer

--- xai_models/imageexplainers.py
+++ xai_models/imageexplainers.py
@@ -6,12 +6,13 @@ from skimage.segmentation import mark_boundaries
 
 from utils.logger import debug, timing, debug_mem, info
 
+
 class LIME():
     def __init__(self, model_path: str):
         super().__init__()
         self.model = tf.keras.models.load_model(model_path)
 
-    def explain(self, image_array: np.ndarray, window: int = 1, should_fit: bool = True, GLOBAL_SEED=0) -> list:
+    def explain(self, image_array: np.ndarray,  GLOBAL_SEED=0) -> list:
         explainer = self.model.LimeImageExplainer(random_state=GLOBAL_SEED)
         explanation = explainer.explain_instance(
             image_array.astype(np.double),
@@ -42,22 +43,34 @@ class LIME():
     #         raise Exception('Signal2vec does not support only approximation. The series has to be transformed firstly')
     #     self.type = method_type
 
-    # def discretize(self, data):
-    #     debug('Length of data {}'.format(len(data)))
-    #     start_time = time.time()
-    #
-    #     pred = self.clf.predict(data.reshape(-1, 1))
-    #
-    #     timing('clf.predict: {}'.format(round(time.time() - start_time, 2)))
-    #     debug('Length of predicted sequence {}'.format(len(pred)))
-    #     debug('Type of discrete sequence {}'.format(type(pred)))
-    #
-    #     return pred
-
     def map_into_vectors(self, sequence):
         start_time = time.time()
         sequence_of_vectors = [self.embedding[str(i)] for i in sequence]
         timing('Appending vectors to list : {}'.format(round(time.time() - start_time, 2)))
         return sequence_of_vectors
 
+
+class SaliencyMap():
+    def __init__(self, model_path: str):
+        super().__init__()
+        self.model = tf.keras.models.load_model(model_path)
+
+    def explain(self, image_array: np.ndarray):
+        img = np.expand_dims(image_array, axis=0)
+        img = tf.convert_to_tensor(img, dtype=tf.float32)
+        result = self.model(img)
+        max_idx = tf.argmax(result, axis=1)
+
+        with tf.GradientTape() as tape:
+            tape.watch(img)
+            result = self.model(img)
+            max_score = result[0, max_idx[0]]
+        grads = tape.gradient(max_score, img)
+
+        return grads[0]
+
+    def normalize_image(self, image_array: np.ndarray,):
+        grads_norm = image_array[:, :, 0] + image_array[:, :, 1] + image_array[:, :, 2]
+        grads_norm = (grads_norm - tf.reduce_min(grads_norm)) / (tf.reduce_max(grads_norm) - tf.reduce_min(grads_norm))
+        return grads_norm
 
